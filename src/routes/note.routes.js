324813ac1b2f38caab5afe0b125ca60a445c6273
Vimukthi Gunarathna <VimukthiG@99x.lk>
2018-07-30 11:37:31 +0530

    fixing require() is not a function error

---
+++ src/routes/note.routes.js
@@ -0,0 +1,26 @@
+
+module.exports =(app) => {
+    try{
+
+    
+    const notes = require('../controllers/note.controller.js');
+
+    //Create a new note
+    app.post('/notes',notes.create);
+
+    //Retrieve all notes
+    app.get('/notes',notes.findAll);
+
+    //Retrieve a single note with noteId
+    app.get('notes/:noteId',notes.findOne);
+
+    //Update a with noteId
+    app.put('/notes/:noteId',notes.update);
+
+    //Delete a Note with noteId
+    app.delete('/notes/:noteId',notes.delete);
+
+    }catch(error){
+        console.log(error)
+    }
+}
